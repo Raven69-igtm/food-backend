e4be098551638ede0f223340e36de832d7bb1396
Abiyyuza <mrafiabi123@gmail.com>
2026-04-28 23:40:10 +0700

    remove google login and use identity verification for reset password

--- internal/router/router.go
+++ internal/router/router.go
@@ -35,7 +35,6 @@ func Setup() *gin.Engine {
 	// --- PUBLIC ROUTES ---
 	r.POST("/api/register", handler.Register)
 	r.POST("/api/login", handler.Login)
-	r.POST("/api/auth/google", handler.GoogleLogin)
 	r.POST("/api/forgot-password", handler.ForgotPassword)
 	r.POST("/api/reset-password", handler.ResetPassword)
 	r.GET("/api/foods", handler.GetProducts)
